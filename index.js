d2abfc8d11959927ff4cc3a7f349fbf1cdae5056
Hiro Kato <hirokikato1@gmail.com>
2023-07-18 22:53:47 -0400

    completes submitData() fetch post

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add your code here
+function submitData(userName, userEmail){
+    return fetch('http://localhost:3000/users', {
+        method: "POST",    
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: userName, 
+            email: userEmail
+        })
+    })
+    .then(res => {return res.json()})
+    .then(data => {
+        console.log(data)
+        document.body.innerHTML = data["id"]
+    })
+    .catch(err => {
+        document.body.innerHTML = err.message
+    })
+}
+
+submitData("test", "testEmail")
